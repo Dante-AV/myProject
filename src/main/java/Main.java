7509da8543e77cb93f5a69f2ab7dd392b4e3e2a9
Anna <mysterious_16@mail.ru>
2022-06-19 19:23:01 +0300

    change gitignore

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,9 @@ import animals.Cat;
 import animals.Dog;
 import animals.Duck;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -15,40 +17,49 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         boolean isExit = true;
         while (isExit) {
-            System.out.println("Select add/list/exit");
-            Command choice = Command.valueOf(sc.next().toUpperCase().trim());
-            switch (choice) {
-                case ADD:
-                    System.out.println("Animal choice: cat/dog/duck");
-                    String animalName = sc.next();
-                    Animal animal;
-                    if (animalName.equals("cat")) {
-                        animal = new Cat();
-                        createAnimal(animal, sc);
-                        animals.add(animal);
-                        animal.say();
-                    } else if (animalName.equals("dog")) {
-                        animal = new Dog();
-                        createAnimal(animal, sc);
-                        animals.add(animal);
-                        animal.say();
-                    } else if (animalName.equals("duck")) {
-                        animal = new Duck();
-                        createAnimal(animal, sc);
-                        animals.add(animal);
-                        animal.say();
-                    }
-
-                    break;
-                case LIST:
-                    animalList(animals);
-                    break;
-                case EXIT:
-                    System.out.println("Exit");
-                    isExit = false;
-                    break;
-                default:
-                    System.out.println("Unknown command");
+            try {
+                System.out.println("Select add/list/exit");
+                Command choice = Command.valueOf(sc.next().toUpperCase().trim());
+                switch (choice) {
+                    case ADD:
+                        System.out.println("Animal choice: cat/dog/duck");
+                        String animalName = sc.next();
+                        Animal animal;
+                        try {
+                            if (animalName.equals("cat")) {
+                                animal = new Cat();
+                                createAnimal(animal, sc);
+                                animals.add(animal);
+                                animal.say();
+                            } else if (animalName.equals("dog")) {
+                                animal = new Dog();
+                                createAnimal(animal, sc);
+                                animals.add(animal);
+                                animal.say();
+                            } else if (animalName.equals("duck")) {
+                                animal = new Duck();
+                                createAnimal(animal, sc);
+                                animals.add(animal);
+                                animal.say();
+                            }
+                            break;
+                        } catch (InputMismatchException e) {
+                            System.out.println("Age or weight is incorrect");
+                        }
+                    case LIST:
+                        if (animals.size() > 0) {
+                            animalList(animals);
+                        } else {
+                            System.out.println("List with animals is empty");
+                        }
+                        break;
+                    case EXIT:
+                        System.out.println("Exit");
+                        isExit = false;
+                        break;
+                }
+            } catch (IllegalArgumentException e) {
+                System.out.println("Unknown command");
             }
         }
     }
@@ -56,10 +67,15 @@ public class Main {
     public static void createAnimal(Animal animal, Scanner scanner) {
         System.out.println("Write name");
         animal.setName(scanner.next());
+        //    try {
         System.out.println("Write age");
         animal.setAge(scanner.nextInt());
         System.out.println("Write weight");
         animal.setWeight(scanner.nextInt());
+//        } catch (Exception e) {
+//            System.out.println("Age or weight is incorrect");
+//            scanner.reset();
+//        }
         System.out.println("Write color");
         animal.setColor(scanner.next());
     }
